334572bce2614d4f7b5b5f2222800be9416dec1b
Matias Federico Nasca <fedenasca@gmail.com>
2019-10-31 22:05:08 -0300

    prueba 1

---
+++ X_VariableSelection.py
@@ -0,0 +1,22 @@
+#empezamos de nuevo
+# bla bla bla bla bla bla
+
+# bla bla bla bla bla bla
+
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+# bla bla bla bla bla bla
+
+# vamos a usar el Git Built-in de VScode
